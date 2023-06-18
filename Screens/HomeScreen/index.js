f102b434996bf4a73c0609b274c1f31776732b0a
joeldeondsouza <jdeondsouza@gmail.com>
2023-06-19 00:57:07 +0200

    Added barCode Scanner function

--- Screens/HomeScreen/index.js
+++ Screens/HomeScreen/index.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, TouchableOpacity, Image, FlatList } from "react-native";
 import { styles } from "./styles";
 // Icons Import //
 import Icon from "react-native-vector-icons/Ionicons";
 // Component //
 import FeaturesList from "./components/FeaturesList";
+// BarCode Scanned //
+import { Camera } from "expo-camera";
 
 const Data = [
   {
@@ -34,6 +36,24 @@ const Data = [
 ];
 
 const HomeScreen = () => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return null;
+  }
+
+  if (hasPermission === false) {
+    return null;
+  }
+
   return (
     <View style={styles.homeContainer}>
       <View style={styles.headerIcons}>
@@ -43,12 +63,20 @@ const HomeScreen = () => {
             source={{ uri: "https://i.ibb.co/jrQLGpG/logo.png" }}
           />
         </TouchableOpacity>
-        <TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            setCameraType(
+              cameraType === Camera.Constants.Type.back
+                ? Camera.Constants.Type.front
+                : Camera.Constants.Type.back
+            );
+          }}
+        >
           <Icon name="ios-qr-code" size={32} style={{ color: "#116D6E" }} />
         </TouchableOpacity>
       </View>
       <View style={styles.headerContainerText}>
-        <Text style={styles.headerText}>High Spot</Text>
+        <Text style={styles.headerText}>Smart Routings</Text>
       </View>
       <View style={styles.flatListContainer}>
         <FlatList
